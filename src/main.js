bd7fb2c4629f59b839291c8e2e93ef31dd5b4783
ndhfar <nadipadhifa5@gmail.com>
2024-06-10 17:26:51 +0700

    Setting up navigation interactive, header background and border bottom change on scroll, adding typed.js in id title

---
+++ src/main.js
@@ -0,0 +1,34 @@
+// header fixed
+window.addEventListener("scroll", () => {
+  const header = document.querySelector("header");
+  const fixedNav = header.offsetTop;
+
+  if (window.scrollY > fixedNav) {
+    header.classList.add("backdrop-blur-lg", "border-b-[1px]");
+  } else {
+    header.classList.remove("backdrop-blur-lg", "border-b-[1px]");
+  }
+});
+
+// navigation
+const menuBtn = document.getElementById("menu-btn");
+const navigation = document.getElementById("navigation");
+
+menuBtn.addEventListener("click", () => {
+  if (menuBtn.classList.contains("bx-menu")) {
+    menuBtn.classList.replace("bx-menu", "bx-x");
+    navigation.classList.remove("hidden");
+  } else if (menuBtn.classList.contains("bx-x")) {
+    menuBtn.classList.replace("bx-x", "bx-menu");
+    navigation.classList.add("hidden");
+  }
+});
+
+// typed.js
+const Typed = require("typed.js");
+
+const typed = new Typed("#title", {
+  strings: ["Fullstack Engineer"],
+  typeSpeed: 1,
+  loop: true,
+});
