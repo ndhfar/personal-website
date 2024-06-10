edd217e2b17369e1db85200cfd22e451a2224b6b
ndhfar <nadipadhifa5@gmail.com>
2024-06-10 17:05:28 +0700

    Adding style custom for font-family, colors, and box-shadow

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+
+      colors: {
+        customWhite: "#EFFFFB",
+        customBlue: "#4F98CA",
+        customBlack: "#272727",
+      },
+
+      boxShadow: {
+        custom: "0 0 10px 0 rgb(0 0 0 / 0.1)",
+      },
+    },
   },
   plugins: [],
 };
-
